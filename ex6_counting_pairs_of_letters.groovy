2294a1fa0a8b001f430f08130976c1688a296390
Sam Wright <swrigh11@dcs.bbk.ac.uk>
2012-10-10 14:17:05 +0100

    Done ex6.

--- ex6_counting_pairs_of_letters.groovy
+++ ex6_counting_pairs_of_letters.groovy
@@ -1,5 +1,34 @@
 print "Enter short string to search for: "
-String str = System.console().readLine()
+String short_str = System.console().readLine()
 
 print "Enter long string to search in: "
-char ch_to_count = System.console().readLine()[0]
+String long_str = System.console().readLine()
+
+int difference_in_lengths = long_str.length() - short_str.length()
+if (difference_in_lengths < 0)
+    throw new BadInput("Short string longer than long string")
+    
+if (short_str.length() < 1)
+    throw new BadInput("Short string is too short")
+    
+int count = 0
+char s_ch, l_ch
+for (long_str_offset in (0..difference_in_lengths)) {
+    boolean match = true
+    for (short_str_index in 0..short_str.length()-1) {
+        //s_ch = 
+        if (short_str[short_str_index] != long_str[long_str_offset+short_str_index]) {
+            match = false
+            break
+        }
+    }
+    if (match)
+        count++
+}
+
+
+println "Found short string " + count + " times in long string."
+/*
+ * NB. I interpretted the problem so that 'aa' would be found twice in 'aaa',
+ * since the exercise didn't prohibit reusing characters.
+ */
