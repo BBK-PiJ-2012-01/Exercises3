11ad09a6eac2d1b495dbae2ed5496a4985fe8da6
Sam Wright <swrigh11@dcs.bbk.ac.uk>
2012-10-10 15:11:46 +0100

    Done ex10.

--- ex10_palindrome_redux.groovy
+++ ex10_palindrome_redux.groovy
@@ -1,6 +1,17 @@
 print "Enter string: "
-String str = System.console().readLine()
+String str = System.console().readLine().toLowerCase()
 
 if (str.length() < 1)
     throw new BadInput("String needs to be at least 1 character long")
     
+String condensed_str = ""
+
+for (ch in str.getChars()) {
+    if (ch.isLetter())
+        condensed_str << ch
+}
+
+if (condensed_str.reverse() == condensed_str)
+    println "That was indeed a relaxed palindrome!"
+else
+    println "Not a relaxed palindrome."
