11470ef04ae7db48ee33436573d61b013c52b05e
Sam Wright <swrigh11@dcs.bbk.ac.uk>
2012-10-10 14:43:16 +0100

    Done ex8.

---
+++ Palindrome.groovy
@@ -0,0 +1,7 @@
+class Palindrome {
+    
+}
+
+print "Enter string: "
+String str = System.console().readLine()
+println Palindrome.reverse(str)
---
+++ ex8_palindrome.groovy
@@ -0,0 +1,6 @@
+print "Enter string: "
+String str = System.console().readLine()
+if (str.reverse() == str)
+    println "That was indeed a strict palindrome!"
+else
+    println "Not a strict palindrome."
